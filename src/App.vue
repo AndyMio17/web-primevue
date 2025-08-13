2836acb4fc8a1c4dc42f9fae80dd63cfd23b9bf5
AndyMio17 <andymiomejia17@gmail.com>
2025-08-13 01:47:03 -0500

    version inicial con PrimeVue y configuracion base

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+
+</script>
+
+<template>
+  <pv-Menubar :model="items">
+    <template #start>
+        <h2>Dashboard</h2>
+        
+    </template>
+
+
+
+    <template #end>
+
+        <div class="flex">
+          <pv-IconField>
+            <pv-InputIcon>
+                <i class="pi pi-search" />
+            </pv-InputIcon>
+            <pv-InputText placeholder="Search" />
+          </pv-IconField>
+
+          <pv-Avatar image="/images/avatar/amyelsner.png" shape="circle" />
+        </div>
+
+    </template>
+  </pv-Menubar>
+
+
+
+
+
+  <div>
+    <h1>holaaa</h1>
+    <pv-Button label="Click Me" icon="pi pi-search" class="p-button"/>
+  </div>
+</template>
+
+<style scoped>
+  .p-button {
+    background-color: #ff9800 ;
+    border-color: #ff9800;
+  }
+  
+  .text{
+    background-color: #ff9800 ;
+    border-color: #ff9800;
+  }
+
+  .flex{
+    display: flex; /* Usar para alinear los elementos*/
+    gap: 10px; /* Espacio entre los elementos */
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,31 @@
+import { createApp } from 'vue'
+
+import App from './App.vue'
+
+// PrimeVue 
+import PrimeVue from 'primevue/config'
+import Button from 'primevue/button'
+import Menubar from 'primevue/menubar'
+import Avatar from 'primevue/avatar'
+import InputText from 'primevue/inputtext'
+import IconField from 'primevue/iconfield';
+import InputIcon from 'primevue/inputicon';
+
+// Estilos de PrimeVue
+import 'primeicons/primeicons.css'  // Iconos
+import Aura from '@primeuix/themes/aura' // Tema Aura oficial de PrimeVue
+
+
+createApp(App)
+    .use(PrimeVue,{
+        theme: {
+            preset: Aura // Cambia el tema aquí
+        }
+    })
+    .component('pv-Button', Button)
+    .component('pv-Menubar', Menubar)
+    .component('pv-Avatar', Avatar)
+    .component('pv-InputText', InputText)
+    .component('pv-IconField', IconField)
+    .component('pv-InputIcon', InputIcon)
+    .mount('#app')
